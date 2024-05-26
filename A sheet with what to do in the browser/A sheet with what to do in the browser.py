d565127505827a0218af6c7e59e9c7f227c68791
Tomashevskyi Vladyslav <113309017+Tomashevskyi-Vladyslav@users.noreply.github.com>
2024-05-27 00:43:26 +0300

    Add files via upload

---
+++ A sheet with what to do in the browser/A sheet with what to do in the browser.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+def main(page):
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        new_task.focus()
+        new_task.update()
+
+    new_task = ft.TextField(hint_text="Whats needs to be done?", width=300)
+    page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
+
+ft.app(target=main, view=ft.WEB_BROWSER)
---
+++ Virtual chat in the browser/Virtual chat in the browser.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Flet Chat"
+
+    # subscribe to broadcast messages
+    def on_message(msg):
+        messages.controls.append(ft.Text(msg))
+        page.update()
+
+    page.pubsub.subscribe(on_message)
+
+    def send_click(e):
+        page.pubsub.send_all(f"{user.value}: {message.value}")
+        # clean up the form
+        message.value = ""
+        page.update()
+
+    messages = ft.Column()
+    user = ft.TextField(hint_text="Your name", width=150)
+    message = ft.TextField(hint_text="Your message...", expand=True)  # fill all the space
+    send = ft.ElevatedButton("Send", on_click=send_click)
+    page.add(messages, ft.Row(controls=[user, message, send]))
+
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
